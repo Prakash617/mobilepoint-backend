1b7205cd1851f5cf092a53395784e767209d2302
Prakash <prakashthapa617@gmail.com>
2025-12-28 01:40:02 +0545

    ..

--- mobilepoint/settings.py
+++ mobilepoint/settings.py
@@ -72,6 +72,9 @@ SIMPLE_JWT = {
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
+    'http://api.gowell.edu.np/',
+    'https://mobilepoint-seven.vercel.app/',
+    
 ]
 
 

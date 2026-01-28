026cc060225ad8d56ece0cb542b4ffe98b9d86e2
Prakash <prakashthapa617@gmail.com>
2026-01-28 13:56:54 +0545

    static and media_url

--- mobilepoint/settings.py
+++ mobilepoint/settings.py
@@ -194,13 +194,15 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-STATIC_URL = "static/"
+STATIC_URL = os.getenv("STATIC_URL")
+# STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-MEDIA_URL = "/media/"
+MEDIA_URL = os.getenv("MEDIA_URL")
+# MEDIA_URL = "/media/"
 
 # DASHUB_SETTINGS = {
 #     "site_logo": "/static/logo.png",

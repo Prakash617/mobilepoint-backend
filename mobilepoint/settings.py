2613e1d813215c640031ae8ef10174df6f666a09
Prakash <prakashthapa617@gmail.com>
2026-01-28 12:57:51 +0545

    update env

--- mobilepoint/settings.py
+++ mobilepoint/settings.py
@@ -18,8 +18,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", default=False)
 
-ALLOWED_HOSTS = ["*"]
-
+ALLOWED_HOSTS = [
+    origin.strip() for origin in os.getenv("ALLOWED_HOSTS", "").split(",")
+]
 
 AUTH_USER_MODEL = "accounts.User"
 X_FRAME_OPTIONS = "SAMEORIGIN"
